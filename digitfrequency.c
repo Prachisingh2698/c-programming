b20b130b33818a68efddec9ea2abe3ee9a986c72
Prachisingh2698 <65603147+Prachisingh2698@users.noreply.github.com>
2020-08-05 17:36:53 +0530

    Add files via upload

---
+++ digitfrequency.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+
+int main() {
+
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */    
+    char ar[1001],i; int j,count;
+    scanf("%s",ar);
+    for(i=48;i<=57;i++)
+    {
+        count=0;
+        for(j=0;j<strlen(ar);j++)
+        {
+            if(ar[j]==i)
+            count++;
+
+        }
+        printf("%d ",count);
+    }
+    return 0;
+}
